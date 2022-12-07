dd319d3d12d78fc4bcfd1da105fe0549b514d760
DPUJA_ANDIKA <diopuja47@students.unnes.ac.id>
2022-12-07 13:01:03 +0700

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 // ignore_for_file: constant_identifier_names
 
-import 'package:capstone_project/pages/about_page.dart';
-import 'package:capstone_project/pages/articles_page.dart';
-import 'package:capstone_project/pages/diary_page.dart';
-import 'package:capstone_project/pages/stress_page.dart';
-import 'package:capstone_project/pages/mental_page.dart';
-import 'package:capstone_project/pages/overthinking_page.dart';
-import 'package:capstone_project/widgets/main_drawer.dart';
+import 'package:capstone_project/presentation/provider/db_provider.dart';
 import 'package:flutter/material.dart';
 
-import 'package:capstone_project/pages/splash_page.dart';
+import 'package:capstone_project/presentation/pages/about_page.dart';
+import 'package:capstone_project/presentation/pages/articles_page.dart';
+import 'package:capstone_project/presentation/pages/diary_page.dart';
+import 'package:capstone_project/presentation/pages/stress_page.dart';
+import 'package:capstone_project/presentation/pages/mental_page.dart';
+import 'package:capstone_project/presentation/pages/overthinking_page.dart';
+import 'package:capstone_project/presentation/widgets/main_drawer.dart';
+import 'package:capstone_project/presentation/pages/splash_page.dart';
 import 'package:capstone_project/common/constant.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,58 +24,61 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application .
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Capstone Project',
-      theme: ThemeData.light().copyWith(
-        colorScheme: kColorScheme,
-        primaryColor: kBlack,
-        scaffoldBackgroundColor: kPrime,
-        textTheme: kTextTheme,
+    return ChangeNotifierProvider(
+      create: (context) => DatabaseProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Capstone Project',
+        theme: ThemeData.light().copyWith(
+          colorScheme: kColorScheme,
+          primaryColor: kBlack,
+          scaffoldBackgroundColor: kPrime,
+          textTheme: kTextTheme,
+        ),
+        home: const SplashPage(),
+        onGenerateRoute: (RouteSettings settings) {
+          switch (settings.name) {
+            case StressPage.ROUTE_NAME:
+              return MaterialPageRoute(
+                builder: (_) => const StressPage(),
+              );
+            case MentalPage.ROUTE_NAME:
+              return MaterialPageRoute(
+                builder: (_) => const MentalPage(),
+              );
+            case ArticlesPage.ROUTE_NAME:
+              return MaterialPageRoute(
+                builder: (_) => const ArticlesPage(),
+              );
+            case DiaryPage.ROUTE_NAME:
+              return MaterialPageRoute(
+                builder: (_) => const DiaryPage(),
+              );
+            case AboutPage.ROUTE_NAME:
+              return MaterialPageRoute(
+                builder: (_) => const AboutPage(),
+              );
+            case OverthinkingPage.ROUTE_NAME:
+              return MaterialPageRoute(
+                builder: (_) => const OverthinkingPage(),
+              );
+            case MainDrawer.ROUTE_NAME:
+              return MaterialPageRoute(
+                builder: (_) => const MainDrawer(),
+              );
+            default:
+              return MaterialPageRoute(
+                builder: (_) {
+                  return const Scaffold(
+                    body: Center(
+                      child: Text('Page not found'),
+                    ),
+                  );
+                },
+              );
+          }
+        },
       ),
-      home: const SplashPage(),
-      onGenerateRoute: (RouteSettings settings) {
-        switch (settings.name) {
-          case StressPage.ROUTE_NAME:
-            return MaterialPageRoute(
-              builder: (_) => const StressPage(),
-            );
-          case MentalPage.ROUTE_NAME:
-            return MaterialPageRoute(
-              builder: (_) => const MentalPage(),
-            );
-          case ArticlesPage.ROUTE_NAME:
-            return MaterialPageRoute(
-              builder: (_) => const ArticlesPage(),
-            );
-          case DiaryPage.ROUTE_NAME:
-            return MaterialPageRoute(
-              builder: (_) => const DiaryPage(),
-            );
-          case AboutPage.ROUTE_NAME:
-            return MaterialPageRoute(
-              builder: (_) => const AboutPage(),
-            );
-          case OverthinkingPage.ROUTE_NAME:
-            return MaterialPageRoute(
-              builder: (_) => const OverthinkingPage(),
-            );
-          case MainDrawer.ROUTE_NAME:
-            return MaterialPageRoute(
-              builder: (_) => const MainDrawer(),
-            );
-          default:
-            return MaterialPageRoute(
-              builder: (_) {
-                return const Scaffold(
-                  body: Center(
-                    child: Text('Page not found'),
-                  ),
-                );
-              },
-            );
-        }
-      },
     );
   }
 }
